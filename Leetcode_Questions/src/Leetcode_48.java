3775f15d7678ee44018fd7f5adb1dbf49e0a0bb1
Abdul Mannan Ansari <abdulmannanansa@gmail.com>
2026-06-25 23:59:18 +0530

    Changes

--- Leetcode_Questions/src/Leetcode_48.java
+++ Leetcode_Questions/src/Leetcode_48.java
@@ -16,6 +16,12 @@ public class Leetcode_48 {
                 matrix[i][n-1-j] = t;
             }
         }
+        /*for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }*/
     }
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
