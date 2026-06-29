4d905bade24828467be905ed14d78f3594b8b45d
Abdul Mannan Ansari <abdulmannanansa@gmail.com>
2026-06-29 16:40:26 +0530

    Minimum Window Substring Problem

--- Leetcode_Questions/src/Leetcode_76.java
+++ Leetcode_Questions/src/Leetcode_76.java
@@ -1,18 +1,48 @@
-/*
 import java.util.HashSet;
 
 public class Leetcode_76 {
 
     public static String minWindow(String s, String t) {
-        HashSet<Character> set = new HashSet<>();
-        int left = 0;
-        for (int right = 0 ; right < s.length() ; right++){
-            while (set.contains(s.charAt(right)))
+        if (s.length() < t.length()) {
+            return "";
         }
+
+        HashSet<Character> set = new HashSet<>();
+        for (char c : t.toCharArray()) {
+            set.add(c);
+        }
+
+        int left = 0, right = 0;
+        int minLength = Integer.MAX_VALUE;
+        int start = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (set.contains(c)) {
+                set.remove(c);
+            }
+            right++;
+
+            while (set.isEmpty()) {
+                if (right - left < minLength) {
+                    minLength = right - left;
+                    start = left;
+                }
+                char leftChar = s.charAt(left);
+                if (t.indexOf(leftChar) != -1) {
+                    set.add(leftChar);
+                }
+                left++;
+            }
+        }
+
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
     }
 
     public static void main(String[] args) {
-        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        String result = minWindow(s, t);
+        System.out.println(result); // Output: "BANC"
     }
 }
-*/
