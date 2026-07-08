a7a542d06e1b4a83cd67baa271634b2e0062f830
Abdul Mannan Ansari <abdulmannanansa@gmail.com>
2026-07-08 22:50:34 +0530

    Concatenate Non-Zero Digits and Multiply by Sum II

---
+++ Leetcode_Questions/src/Leetcode_3756.java
@@ -0,0 +1,55 @@
+//F*CK ASS PROBLEM
+//WATCH VIDEO
+
+public class Leetcode_3756 {
+    public int[] sumAndMultiply(String s, int[][] queries) {
+        long MOD = 1_000_000_007;
+        int len = s.length();
+
+        long[] preSum = new long[len + 1];
+        long[] preProduct = new long[len + 1];
+        int[] nonZeroCnt = new int[len + 1];
+        long[] p10 = new long[len + 1];
+
+        p10[0] = 1;
+        for (int i = 0; i < len; i++) {
+            p10[i + 1] = (p10[i] * 10) % MOD;
+
+            int digit = s.charAt(i) - '0';
+            preSum[i + 1] = preSum[i] + digit;
+
+            if (digit == 0) {
+                preProduct[i + 1] = preProduct[i];
+                nonZeroCnt[i + 1] = nonZeroCnt[i];
+            } else {
+                preProduct[i + 1] = (preProduct[i] * 10 + digit) % MOD;
+                nonZeroCnt[i + 1] = nonZeroCnt[i] + 1;
+            }
+        }
+
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+
+            long sum = preSum[end + 1] - preSum[start];
+            int cnt = nonZeroCnt[end + 1] - nonZeroCnt[start];
+
+            long subtract = (preProduct[start] * p10[cnt]) % MOD;
+            long x = (preProduct[end + 1] - subtract + MOD) % MOD;
+
+            res[i] = (int) ((x * sum) % MOD);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Leetcode_3756 solution = new Leetcode_3756();
+        String s = "123405";
+        int[][] queries = {{0, 5}, {1, 3}, {2, 4}};
+        int[] result = solution.sumAndMultiply(s, queries);
+        for (int res : result) {
+            System.out.println(res);
+        }
+    }
+}
